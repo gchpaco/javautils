18bbbe3a66332333510c4a95bd5cf0236d17bedc
Graham Hughes <graham@sigwinch.org>
2006-07-19 09:56:11 +0000

    [javautils @ Add a red-black set implementation]

---
+++ src/util/purefun/Set.java
@@ -0,0 +1,7 @@
+package util.purefun;
+
+public interface Set<T>
+{
+    public Set<T> insert(T t);
+    public boolean member(T t);
+}
---
+++ tests/util/purefun/RedBlackTest.java
@@ -0,0 +1,31 @@
+package util.purefun;
+
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+
+@Test
+public class RedBlackTest {
+    public void simplePersistence() throws EmptyException {
+	Set<Integer> set = RedBlackSet.empty();
+	Set<Integer> set2 = set.insert(5);
+	assertTrue(set2.member(5));
+	assertFalse(set.member(5));
+	Set<Integer> set3 = set2.insert(2);
+	assertTrue(set3.member(2));
+	assertFalse(set2.member(2));
+	assertFalse(set.member(2));
+	assertTrue(set3.member(5));
+	assertTrue(set2.member(5));
+	assertFalse(set.member(5));
+    }
+    public void insertion() {
+	int [] testData = { 6, 4, 2, 7, 8, 1, 3, 0, 6, 22 };
+	Set<Integer> set = RedBlackSet.empty();
+	for (int i : testData)
+	    assertFalse(set.member(i));
+	for (int i : testData)
+	    set = set.insert(i);
+	for (int i : testData)
+	    assertTrue(set.member(i));
+    }
+}
