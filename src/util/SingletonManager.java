11e105ff6427d554cb472dd89bbfa7db4524f670
Graham Hughes <graham@sigwinch.org>
2006-11-06 07:02:49 +0000

    [javautils @ Reformat singleton class]

--- src/util/SingletonManager.java
+++ src/util/SingletonManager.java
@@ -3,28 +3,39 @@ package util;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
-public class SingletonManager {
+public class SingletonManager
+  {
     public static ConcurrentMap<Class<?>, Object> singletonMap;
-    
-    static {
-        singletonMap = new ConcurrentHashMap<Class<?>, Object>();
-    }
-    
-    @SuppressWarnings("unchecked")
-    public static <T> T get(Class<T> klass) {
-        if (!singletonMap.containsKey(klass)) {
-            throw new IllegalArgumentException ("No instance of " + klass.getCanonicalName() + " registered");
-        }
-        return (T) singletonMap.get(klass);
-    }
-    
-    @SuppressWarnings("unchecked")
-    public static <T, U extends T> T register(Class<T> klass, U object) {
-        singletonMap.putIfAbsent(klass, object);
-        return (T) singletonMap.get(klass);
-    }
-    
-    public static void clear () {
-        singletonMap.clear();
-    }
-}
+
+    static
+      {
+        singletonMap = new ConcurrentHashMap<Class<?>, Object> ();
+      }
+
+    @SuppressWarnings ("unchecked")
+    public static <T> T get (Class<T> klass)
+      {
+        if (singletonMap.containsKey (klass))
+          return (T) singletonMap.get (klass);
+        throw new IllegalArgumentException ("No instance of "
+                                            + klass.getName ()
+                                            + " registered");
+      }
+
+    @SuppressWarnings ("unchecked")
+    public static <T, U extends T> T register (Class<T> klass, U object)
+      {
+        singletonMap.putIfAbsent (klass, object);
+        return (T) singletonMap.get (klass);
+      }
+
+    public static void clear ()
+      {
+        singletonMap.clear ();
+      }
+
+    public static boolean has (Class<?> name)
+      {
+        return singletonMap.containsKey (name);
+      }
+  }
