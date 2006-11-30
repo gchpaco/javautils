7c40305861742a8bc296e4d0c036c7dc26262af8
Graham Hughes <graham@sigwinch.org>
2006-11-30 03:28:25 +0000

    [javautils @ Reformat constant function]

--- src/util/Functions.java
+++ src/util/Functions.java
@@ -2,9 +2,14 @@ package util;
 
 public final class Functions
   {
-    static public <T,U> Function<T,U> constant (final T t) {
-      return new Function<T,U> () {
-        public T apply (U arg) { return t; }
-      };
-    }
+    static public <T,U> Function<T,U> constant (final T t)
+      {
+        return new Function<T,U> ()
+          {
+            public T apply(U arg)
+              {
+                return t;
+              }
+          };
+      }
   }
