1ade546218702f1241711542d716f6eaba135564
Graham Hughes <graham@sigwinch.org>
2006-12-08 22:42:25 +0000

    [javautils @ Remove useless and confusing creation method] I've been burned more times by the fact that L.ist () of a collection does not provide a list of a collection than I can readily count.  Removed.

--- src/util/L.java
+++ src/util/L.java
@@ -6,11 +6,7 @@ package util;
 import java.util.*;
 
 public class L {
-	public static <T> List<T> ist (Collection<T> c) {
-		return new ArrayList<T> (c);
-	}
-
-	public static <T> List<T> ist (T... o) {
-		return ist (Arrays.asList (o));
-	}
+  public static <T> List<T> ist (T... o) {
+    return new ArrayList<T> (Arrays.asList (o));
+  }
 }
--- src/util/S.java
+++ src/util/S.java
@@ -4,14 +4,9 @@ import java.util.*;
 
 public class S
   {
-    public static <T> Set<T> et (Collection<T> c)
-      {
-        return new HashSet<T> (c);
-      }
-
     public static <T> Set<T> et (T... o)
       {
-        return et (Arrays.asList (o));
+        return new HashSet<T> (Arrays.asList (o));
       }
 
   }
