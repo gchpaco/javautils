24c7e6ace400ded38cb47809871837103427692e
Graham Hughes <graham@sigwinch.org>
2007-01-25 12:07:40 +0000

    [javautils @ Add some StringTemplate related utilities]

---
+++ src/util/StringTemplateClient.java
@@ -0,0 +1,11 @@
+package util;
+
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
+
+public interface StringTemplateClient
+  {
+
+    public abstract StringTemplate writeUsing (StringTemplateGroup templates);
+
+  }
---
+++ src/util/StringTemplateUtilities.java
@@ -0,0 +1,22 @@
+package util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.stringtemplate.StringTemplate;
+import org.antlr.stringtemplate.StringTemplateGroup;
+
+public abstract class StringTemplateUtilities
+  {
+
+    public static List<StringTemplate> listToTemplates (
+                                                        List<? extends StringTemplateClient> list,
+                                                        StringTemplateGroup templates)
+      {
+        List<StringTemplate> result = new ArrayList<StringTemplate> ();
+        for (StringTemplateClient stc : list)
+          result.add (stc.writeUsing (templates));
+        return result;
+      }
+
+  }
