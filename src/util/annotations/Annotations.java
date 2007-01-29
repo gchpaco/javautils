50974bf2525055fff38e32971aa50ec17877910e
Graham Hughes <graham@sigwinch.org>
2007-01-29 12:05:22 +0000

    [javautils @ Add way of getting all methods with a given annotation]

--- src/util/annotations/Annotations.java
+++ src/util/annotations/Annotations.java
@@ -1,9 +1,20 @@
 package util.annotations;
 import java.lang.annotation.*;
 import java.lang.reflect.*;
+import java.util.*;
 
 public class Annotations
 {
+    public static List<Method> getAnnotatedMethods (Class<?> target,
+					            Class<? extends Annotation>
+					            annotation)
+    {
+        List<Method> methods = new ArrayList<Method> ();
+	for (Method m : target.getMethods ())
+	    if (m.isAnnotationPresent (annotation))
+		methods.add (m);
+	return methods;
+    }
     public static Method getAnnotatedMethod (Class<?> target,
 					     Class<? extends Annotation>
 					     annotation)
