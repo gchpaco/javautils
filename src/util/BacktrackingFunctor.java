60d49342b28ca9a71814137c21a3b83b7ca10adc
Graham Hughes <graham@sigwinch.org>
2007-10-01 22:31:58 -0700

    Use correct name for Backtracking Functors

--- src/util/BacktrackingFunctor.java
+++ src/util/BacktrackingFunctor.java
@@ -5,7 +5,7 @@ package util;
 
 import net.sf.jga.fn.BinaryFunctor;
 
-public abstract class BacktrackingFunction<T, U> extends BinaryFunctor<T, U, T>
+public abstract class BacktrackingFunctor<T, U> extends BinaryFunctor<T, U, T>
 {
   public abstract T empty ();
 }
