a7e8b77d412647cd7acaf70cd1eabfafe5775d01
Graham Hughes <graham@sigwinch.org>
2008-01-01 05:22:30 -0800

    Add some default implementations to the closure types.

--- src/util/parser/igc/CloseScope.java
+++ src/util/parser/igc/CloseScope.java
@@ -4,6 +4,12 @@ import net.sf.jga.fn.Generator;
 
 public abstract class CloseScope extends Generator<Object>
 {
+  public Object gen ()
+  {
+    symbols.pop ();
+    return null;
+  }
+    
   @Override
   public String toString () { return "pop old scope"; }
 }
--- src/util/parser/igc/OpenScope.java
+++ src/util/parser/igc/OpenScope.java
@@ -4,6 +4,12 @@ import net.sf.jga.fn.Generator;
 
 public abstract class OpenScope extends Generator<Object>
 {
+  public Object gen ()
+  {
+    symbols.push ();
+    return null;
+  }
+    
   @Override
   public String toString () { return "push new scope"; }
 }
