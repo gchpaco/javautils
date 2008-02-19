a4a3940c3374d343fad35bc40afa8c2fe654acc3
Graham Hughes <graham@cs.ucsb.edu>
2008-02-19 01:00:40 -0800

    Accomodate current JPF brokenness as far as annotations go.  This differs from past JPF brokenness in some subtle ways; first, getMethods is wrong but getDeclaredMethods works fine, so we have to walk the inheritance tree ourselves.  Second of the various annotation methods only getAnnotation () is really reliable (yes, Dorothy, getAnnotation (anAnnotation) works when isAnnotationPresent doesn't).

--- src/util/annotations/Annotations.java
+++ src/util/annotations/Annotations.java
@@ -10,27 +10,37 @@ public class Annotations
 					            annotation)
     {
         List<Method> methods = new ArrayList<Method> ();
-	for (Method m : target.getMethods ())
-	    if (m.isAnnotationPresent (annotation))
-		methods.add (m);
+	// if we could use getMethods () from within JPF, the following
+	// recursion would not be necessary.  Sigh.
+	for (Method m : target.getDeclaredMethods ())
+	    if (m.getAnnotation (annotation) != null)
+	        methods.add (m);
+	Class sup = target.getSuperclass ();
+	if (sup != null) methods.addAll (getAnnotatedMethods (sup, annotation));
 	return methods;
     }
     public static Method getAnnotatedMethod (Class<?> target,
 					     Class<? extends Annotation>
 					     annotation)
     {
-	for (Method m : target.getMethods ())
-	    if (m.isAnnotationPresent (annotation))
-		return m;
-	return null;
+	// if we could use getMethods () from within JPF, the following
+	// recursion would not be necessary.  Sigh.
+	for (Method m : target.getDeclaredMethods ())
+	    if (m.getAnnotation (annotation) != null)
+	        return m;
+	Class sup = target.getSuperclass ();
+	return sup == null ? null : getAnnotatedMethod (sup, annotation);
     }
     public static Field getAnnotatedField (Class<?> target,
 					   Class<? extends Annotation>
 					   annotation)
     {
-	for (Field f : target.getFields ())
-	    if (f.isAnnotationPresent (annotation))
-		return f;
-	return null;
+	// if we could use getFields () from within JPF, the following
+	// recursion would not be necessary.  Sigh.
+	for (Field f : target.getDeclaredFields ())
+	    if (f.getAnnotation (annotation) != null)
+	        return f;
+	Class sup = target.getSuperclass ();
+	return sup == null ? null : getAnnotatedField (sup, annotation);
     }
 }
