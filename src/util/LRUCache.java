d40d85ad5e88d7f1f58dcda459c1939b58ac6774
Graham Hughes <graham@sigwinch.org>
2008-10-27 17:52:44 -0700

    Add a closure operation to the LRU cache

--- src/util/LRUCache.java
+++ src/util/LRUCache.java
@@ -2,6 +2,8 @@ package util;
 
 import java.util.LinkedHashMap;
 
+import net.sf.jga.fn.Generator;
+
 public class LRUCache<K, V> extends LinkedHashMap<K, V>
 {
   private int max = 10;
@@ -16,5 +18,17 @@ public class LRUCache<K, V> extends LinkedHashMap<K, V>
   protected boolean removeEldestEntry (java.util.Map.Entry<K, V> eldest)
     {
       return size () > max;
+    }
+
+  public V get (K i, Generator<V> closure)
+    {
+      if (containsKey (i))
+        return get (i);
+      else
+        {
+          V var = closure.fn ();
+          put (i, var);
+          return var;
+        }
     }
 }
--- tests/util/LRUCacheTest.java
+++ tests/util/LRUCacheTest.java
@@ -1,16 +1,26 @@
 package util;
 
+import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
+import net.sf.jga.fn.Generator;
 
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 @Test
 public class LRUCacheTest
 {
+  private LRUCache<Integer, Integer> cache;
+
+  @BeforeTest
+  public void setUp ()
+    {
+      cache = new LRUCache<Integer, Integer> (1, 3);
+    }
+
   public void simple ()
     {
-      LRUCache<Integer, Integer> cache = new LRUCache<Integer, Integer> (1, 3);
       cache.put (1, 42);
       cache.put (3, 52);
       cache.get (1);
@@ -20,5 +30,26 @@ public class LRUCacheTest
       assertTrue (cache.containsKey (1));
       assertTrue (cache.containsKey (2));
       assertTrue (cache.containsKey (4));
+    }
+
+  public void closure ()
+    {
+      final int[] timesRun = { 0 };
+
+      Generator<Integer> closure = new Generator<Integer> ()
+        {
+          @Override
+          public Integer gen ()
+            {
+              return timesRun[0]++;
+            }
+        };
+      assertEquals ((int) cache.get (1, closure), 0);
+      assertEquals ((int) cache.get (2, closure), 1);
+      assertEquals ((int) cache.get (1, closure), 0);
+      assertEquals ((int) cache.get (3, closure), 2);
+      assertEquals ((int) cache.get (4, closure), 3);
+      assertEquals (timesRun[0], 4);
+      assertFalse (cache.containsKey (2));
     }
 }
